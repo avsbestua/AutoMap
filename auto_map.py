7187e745ce1c0676dd0356b3c50f3bb69156c5d6
bibed234 <vit.lytovchenko@gmail.com>
2025-07-28 12:50:13 +0300

    bug fix

--- auto_map.py
+++ auto_map.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageDraw, ImageFont
-import constants
+import constants, random
 
 img = Image.open(r".\map.png").convert("RGBA")
 draw = ImageDraw.Draw(img)
@@ -7,22 +7,25 @@ draw = ImageDraw.Draw(img)
 
 for name, points in constants.countries.items():
     try:
-        info = constants.information[name]
-        color = constants.filling[info]
+        # info = constants.information[name]
+        # color = constants.filling[info]
+        pass
     except:
-        info = ''
+        # info = ''
         # color = (255, 255, 255, 255)
-        continue
-
+        # continue
+        pass
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), 255)
+    info = ""
     for coord in points:
-        ImageDraw.floodfill(img, xy=coord, value=color, thresh=200)
+        ImageDraw.floodfill(img, xy=coord, value=color, thresh=250)
 
     if name in ['luxembourg', 'cyprus', 'kosovo', 'slovenia']:
         continue #Next iteration if name in list
 
     if name in ['ukraine', 'poland', 'germany', 'france',
                 'spain', 'parasha', 'finland', 'sweden',
-                'turkey', 'romania']:
+                'turkey', 'romania','italy',]:
         size = 60
     else:
         size = 25
