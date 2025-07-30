46a2ae6f96554a4a8c0c2ce54a6f2e998542f468
avsbestua <rmetrikovskij@gmail.com>
2025-07-30 13:32:29 +0300

    Refactor color assignment logic and update filling ranges
    Replaces random color assignment in auto_map.py with logic based on ai_answer and the filling dictionary. Updates the filling dictionary in constants.py to use new value ranges and color tuples, removing alpha values and redundant entries.

--- auto_map.py
+++ auto_map.py
@@ -4,7 +4,7 @@ import constants, ai, random
 img = Image.open(r".\map2.png").convert("RGB")
 draw = ImageDraw.Draw(img)
 
-#ai_answer = ai.ai_request() #Requesting information from AI
+ai_answer = ai.ai_request() #Requesting information from AI
 # if constants.text_mode:
 #     countries = constants.countries_text
 # else:
@@ -12,13 +12,17 @@ draw = ImageDraw.Draw(img)
 
 for name, points in constants.countries.items():
     try:
-        # info = ai_answer[name]
-        # for (low_lim, high_lim), color_tup in constants.filling.items():
-        #     info = int(info)
-        #     if low_lim <= info <= high_lim:
-        #         color = color_tup
-        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        info = random.randint(0, 5)
+        info = ai_answer[name]
+        for (low_lim, high_lim), color_tup in constants.filling.items():
+            info = int(info)
+            if low_lim <= info <= high_lim:
+                color = color_tup
+            else:
+                color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+
+        # color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        # info = random.randint(0, 5)
     except:
         pass
 
