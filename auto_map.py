aee54df82b4011a2ea4a20c4cf228077a1290c49
avsbestua <rmetrikovskij@gmail.com>
2025-07-30 11:51:16 +0300

    уцкцкцкцц

--- auto_map.py
+++ auto_map.py
@@ -1,25 +1,26 @@
 from PIL import Image, ImageDraw, ImageFont
 import constants, ai, random
 
-img = Image.open(r".\map.png").convert("RGBA")
+img = Image.open(r".\map2.png").convert("RGB")
 draw = ImageDraw.Draw(img)
 
-ai_answer = ai.ai_request() #Requesting information from AI
-if constants.text_mode:
-    countries = constants.countries_text
-else:
-    countries = constants.countries
+#ai_answer = ai.ai_request() #Requesting information from AI
+# if constants.text_mode:
+#     countries = constants.countries_text
+# else:
+#     countries = constants.countries
 
-for name, points in countries.items():
+for name, points in constants.countries.items():
     try:
-        info = ai_answer[name]
-        for (low_lim, high_lim), color_tup in constants.filling.items():
-            info = int(info)
-            if low_lim <= info <= high_lim:
-                color = color_tup
-
+        # info = ai_answer[name]
+        # for (low_lim, high_lim), color_tup in constants.filling.items():
+        #     info = int(info)
+        #     if low_lim <= info <= high_lim:
+        #         color = color_tup
+        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        info = random.randint(0, 5)
     except:
-        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), 255)
+        pass
 
 
     for coord in points:
