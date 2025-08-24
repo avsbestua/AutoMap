097fd5b4593d71be9c9a2c7490dea5c03c0124d5
avsbestua <rmetrikovskij@gmail.com>
2025-08-24 20:10:14 +0300

    +63

--- video/chromakey_utils.py
+++ video/chromakey_utils.py
@@ -1,33 +1,33 @@
-# from moviepy import VideoFileClip
-# import os
-#
-# video = VideoFileClip("sub_button.mp4")
-# os.makedirs("frames", exist_ok=True)
-#
-# for i, frame in enumerate(video.iter_frames(fps=video.fps, dtype='uint8')):
-#     from PIL import Image
-#     img = Image.fromarray(frame)
-#     img.save(f"frames/frame_{i:04d}.png")
+from moviepy import VideoFileClip
+import os
 
-# from PIL import Image
-# import os
-#
-# frame_files = sorted(os.listdir("frames"))
-# os.mkdir('frames_alpha')
-# for file in frame_files:
-#     img = Image.open(f"frames/{file}").convert("RGBA")
-#     datas = img.getdata()
-#
-#     newData = []
-#     for item in datas:
-#         # Якщо чорний піксель, робимо прозорим
-#         if item[0] == 0 and item[1] == 254 and item[2] == 0:
-#             newData.append((0, 0, 0, 0))
-#         else:
-#             newData.append(item)
-#
-#     img.putdata(newData)
-#     img.save(f"frames_alpha/{file}")
+video = VideoFileClip("European_stars.svg.mp4")
+os.makedirs("frames", exist_ok=True)
+
+for i, frame in enumerate(video.iter_frames(fps=video.fps, dtype='uint8')):
+    from PIL import Image
+    img = Image.fromarray(frame)
+    img.save(f"frames/frame_{i:04d}.png")
+
+from PIL import Image
+import os
+
+frame_files = sorted(os.listdir("frames"))
+os.makedirs('frames_alpha', exist_ok=True)
+for file in frame_files:
+    img = Image.open(f"frames/{file}").convert("RGBA")
+    datas = img.getdata()
+
+    newData = []
+    for item in datas:
+        # Якщо чорний піксель, робимо прозорим
+        if item[0] < 10 and item[1] < 10 and item[2] < 10:
+            newData.append((0, 0, 0, 0))
+        else:
+            newData.append(item)
+
+    img.putdata(newData)
+    img.save(f"frames_alpha/{file}")
 
 from moviepy import ImageSequenceClip
 import os
@@ -36,8 +36,8 @@ frame_files = sorted(os.listdir("frames_alpha"))
 frame_paths = [os.path.join("frames_alpha", f) for f in frame_files]
 
 # fps беремо, наприклад, 30, або той, що був у оригінальному відео
-clip = ImageSequenceClip(frame_paths, fps=60)
+clip = ImageSequenceClip(frame_paths, fps=30)
 
 # Зберігаємо відео з альфа-каналом (mov + png codec підтримує прозорість)
-clip.write_videofile("sub_button.mov", codec="png", fps=60)
+clip.write_videofile("stars.mov", codec="png", fps=30)
 
--- video/video_edit.py
+++ video/video_edit.py
@@ -1,8 +1,11 @@
-from moviepy import VideoFileClip, TextClip, CompositeVideoClip, ImageClip
-import numpy as np
+from moviepy import VideoFileClip, TextClip, CompositeVideoClip, ImageClip, AudioFileClip
+
 
 def video_edit(theme: str):
-    bg = VideoFileClip('bg.mp4').with_duration(2)
+    bg = VideoFileClip('bg.mp4').with_duration(15)
+    music = AudioFileClip('evint2.mp3').with_duration(bg.duration)
+    bg = bg.with_audio(music)
+
     text = TextClip(text=theme,
                     font='font.ttf',
                     font_size=30,
@@ -15,13 +18,13 @@ def video_edit(theme: str):
                     ).with_duration(bg.duration).with_position((50, 60))
 
     water_mark = TextClip(text="FutureGeo",
-                    font='font.ttf',
-                    font_size=30,
-                    color='#8603a7',
-                    size=(bg.w - 100, None),
-                    text_align='center',
-                    method='caption',
-                    ).with_duration(bg.duration).with_position(('center', bg.h - 160)).with_opacity(0.5)
+                          font='font.ttf',
+                          font_size=30,
+                          color='#8603a7',
+                          size=(bg.w - 100, None),
+                          text_align='center',
+                          method='caption',
+                          ).with_duration(bg.duration).with_position(('center', bg.h - 160)).with_opacity(0.5)
 
     stars = VideoFileClip('stars.mov', has_mask=True).with_duration(bg.duration).resized(0.2).with_position((140, 10))
     stars.with_position('top', 'center')
@@ -29,8 +32,6 @@ def video_edit(theme: str):
     map_img = ImageClip(r'../img.png').with_duration(bg.duration).with_position('center').resized(width=bg.w)
 
     subscribe = VideoFileClip('sub_button.mov', has_mask=True).with_position((-160, 115)).resized(0.25)
-
-
 
     video = CompositeVideoClip([bg, stars, text, map_img, water_mark, subscribe])
 
