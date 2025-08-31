b30b46b90d7944d4695967c869974c810daa4371
avsbestua <rmetrikovskij@gmail.com>
2025-09-01 00:02:12 +0300

    gui update

--- map/gui.py
+++ map/gui.py
@@ -14,14 +14,16 @@ def map_(entry_get, var, map_var):  # Create threading for function auto_map
 
 class App:
     def __init__(self):
+        bg_color = '#E4572E'
+        fg_color = '#2E282A'
         root = tk.Tk()
-        root['bg'] = '#388E3C'
+        root['bg'] = bg_color
         root.title("AutoMap")
         root.iconbitmap(r'resources/favicon.ico')
         root.geometry("450x250+500+200")
         root.resizable(width=False, height=False)
 
-        tk.Label(root, text="Welcome to AutoMap", font=("Consolas Bold", 25), bg='#388E3C', fg='#ffffff').pack()
+        tk.Label(root, text="Welcome to AutoMap!", font=("Consolas Bold", 25), bg=bg_color, fg=fg_color).pack()
 
         entry = tk.Entry(root, font=("Consolas Bold", 25))
         entry.pack(pady=5)
@@ -32,11 +34,11 @@ class App:
         rd_frame_mode = tk.Frame(root)  # Frame for radio buttons
         rd_frame_mode.pack()
 
-        rd1 = tk.Radiobutton(rd_frame_mode, text="Text mode", variable=var, value="txt", bg="#388E3C", fg='#ffffff',
+        rd1 = tk.Radiobutton(rd_frame_mode, text="Text mode", variable=var, value="txt", bg=bg_color, fg=fg_color,
                              font=("Consolas Bold", 15))
         rd1.pack(side='left')
 
-        rd2 = tk.Radiobutton(rd_frame_mode, text="Number mode", variable=var, value="num", bg="#388E3C", fg='#ffffff',
+        rd2 = tk.Radiobutton(rd_frame_mode, text="Number mode", variable=var, value="num", bg=bg_color, fg=fg_color,
                              font=("Consolas Bold", 15))
         rd2.pack()
 
@@ -45,20 +47,20 @@ class App:
         rd_frame_map = tk.Frame(root)
         rd_frame_map.pack()
 
-        rd_def_map = tk.Radiobutton(rd_frame_map, text="Default map", variable=map_var, value="default", bg="#388E3C", fg='#ffffff',
+        rd_def_map = tk.Radiobutton(rd_frame_map, text="Default map", variable=map_var, value="default", bg=bg_color, fg=fg_color,
                                     font=("Consolas Bold", 15))
         rd_def_map.pack(side='left')
 
-        rd_flag_map = tk.Radiobutton(rd_frame_map, text="Flag map", variable=map_var, value='flag', bg="#388E3C", fg='#ffffff',
+        rd_flag_map = tk.Radiobutton(rd_frame_map, text="Flag map", variable=map_var, value='flag', bg=bg_color, fg=fg_color,
                                      font=("Consolas Bold", 15))
         rd_flag_map.pack(side='left')
 
-        rd_world_map = tk.Radiobutton(rd_frame_map, text="World map", variable=map_var, value='world', bg="#388E3C", fg='#ffffff',
+        rd_world_map = tk.Radiobutton(rd_frame_map, text="World map", variable=map_var, value='world', bg=bg_color, fg=fg_color,
                                      font=("Consolas Bold", 15))
 
         rd_world_map.pack(side='left')
 
-        button = tk.Button(root, text="Start AutoMap", font=("Consolas Bold", 25), bg='#8BC34A', fg='#ffffff',
+        button = tk.Button(root, text="Start AutoMap", font=("Consolas Bold", 25), bg='#FFC914', fg=fg_color,
                            command=lambda: map_(entry.get(), var, map_var))
         button.pack(pady=5)
 
